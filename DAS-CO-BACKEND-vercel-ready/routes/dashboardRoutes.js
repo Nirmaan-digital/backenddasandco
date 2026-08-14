1ceaa0c40cda30ba719078865de9412f66071b19
nirmaandigital <nirmaandigital01@gmail.com>
2026-08-14 07:50:21 +0530

    delete old files

--- DAS-CO-BACKEND-vercel-ready/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getDashboard } = require("../controllers/dashboardController");
-
-// GET Dashboard Data
-router.get("/", getDashboard);
-
-module.exports = router;
